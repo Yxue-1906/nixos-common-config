28604e3753e583d4e9fcc1051c84e227ab00b627
nanashi <na@na.shi>
2025-07-23 00:41:20 +0800

    feat: add xclip to add clipboard support for console applications on x11

--- applications/editors.nix
+++ applications/editors.nix
@@ -1,10 +1,15 @@
 { pkgs, ... }: {
+  # GH-14: Add clipboard support
+  environment.systemPackages = [
+    pkgs.xclip
+  ];
   programs = {
     nano.enable = false;
     neovim = {
       enable = true;
       defaultEditor = true;
       vimAlias = true;
+      viAlias = true;
     };
   };
 }
