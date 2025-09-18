65dbb9b7d700b63692e028cafe68e152ded04fee
nanashi <na@na.shi>
2025-09-19 00:44:36 +0800

    feat: update flake input, update git hook

--- flake.nix
+++ flake.nix
@@ -8,11 +8,11 @@
     ];
   };
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-shared-flakes.url = "github:Yxue-1906/nixos-shared-flake";
     nixpkgs-sing-box.url = "github:NixOS/nixpkgs/0ae51d2e8e7fbb16046111569a5b0378aa85a129";
     # nix-alien.url = "github:thiagokokada/nix-alien";
   };
-  outputs = { self, nixpkgs, ... }@inputs: with nixpkgs.lib; {
+  outputs = { self, nixos-shared-flakes, ... }@inputs: with nixos-shared-flakes.nixpkgs.lib; {
     build-host = { profile, secrets, ... }: {
       nixosConfigurations."unrelated" = nixosSystem rec {
 	# now set system manually is work around, find if can use nixpkgs.hostPlatform
