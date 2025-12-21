ec704a43040d6ff33bf740804979e0bdbfc522e2
nanashi <na@na.shi>
2025-12-21 10:09:03 +0800

    fix: disable gcr ssh agent, remove useless aria2 overlay

--- applications/aria2.nix
+++ applications/aria2.nix
@@ -1,15 +1,4 @@
 { pkgs, ... }: {
-  nixpkgs.overlays = [
-    (final: prev: {
-      # disable bittorrent and metalink
-      aria2 = prev.aria2.overrideAttrs (old: {
-        configureFlags = (old.configureFlags or []) ++ [
-	  "--disable-bittorrent"
-	  "--disable-metalink"
-	];
-      });
-    })
-  ];
   # Enable local ariang server
   services.nginx.virtualHosts.localhost = {
     locations."/ariang/" = {
--- applications/gnupg.nix
+++ applications/gnupg.nix
@@ -6,21 +6,7 @@
   # disable ssh-agent from gnome-keyring
   # - starting from 25.11
   # see: https://github.com/NixOS/nixpkgs/pull/379731
-  # services.gnome.gcr-ssh-agent.enable = false;
-  # - till 25.05
-  nixpkgs.overlays = [
-    # https://github.com/NixOS/nixpkgs/blob/nixos-24.11/pkgs/by-name/gn/gnome-keyring/package.nix
-    # To disable SSH_AUTH_SOCK by gnome-keyring.
-    #
-    # And it should be override the package it self, the module is not configurable for the package. https://github.com/NixOS/nixpkgs/blob/nixos-24.11/nixos/modules/services/desktops/gnome/gnome-keyring.nix
-    (final: prev: {
-      gnome-keyring = prev.gnome-keyring.overrideAttrs (
-        finalAttrs: prevAttrs: {
-	  mesonFlags = final.lib.lists.remove "-Dssh-agent=true" prevAttrs.mesonFlags;
-	}
-      );
-    })
-  ];
+  services.gnome.gcr-ssh-agent.enable = false;
 
   services.pcscd.enable = true;
   programs.gnupg.agent = {
